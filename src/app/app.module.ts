883abb13c7d65d3b293f8e0d758b06ce848caf85
dev3 <impltech.team@ukr.net>
2018-09-20 10:37:34 +0300

    navbar html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import {BootstrapOptions} from "../../node_modules/@angular/core/src/application_ref";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
