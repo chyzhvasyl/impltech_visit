734afee67893f705fd04f0a127fd8a6d4ca8d37d
dev3 <impltech.team@ukr.net>
2018-09-21 19:02:40 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import {BootstrapOptions} from "../../node_modules/@angular/core/src/application_ref";
+import { ParallaxDirective } from './components/parallax.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    ParallaxDirective
   ],
   imports: [
     BrowserModule
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import $ from "jquery";
+
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit() {
+
+
   }
 
 }
